3a2e834a23b736ebecebad1581a26d8694288e30
RAVANREX <40683051+RAVANREX@users.noreply.github.com>
2020-08-16 17:21:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('models/'),
-  faceapi.nets.faceExpressionNet.loadFromUri('models/')
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://github.com/justadudewhohacks/face-api.js/tree/master/weights'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://github.com/justadudewhohacks/face-api.js/tree/master/weights'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://github.com/justadudewhohacks/face-api.js/tree/master/weights'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://github.com/justadudewhohacks/face-api.js/tree/master/weights')
 ]).then(startVideo)
 
 function startVideo() {
